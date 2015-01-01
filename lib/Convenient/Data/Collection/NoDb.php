4fa55f63885c3484ece80ffc2ac476d7baed14e3
convenient <lukerodgers90@gmail.com>
2015-01-01 20:27:17 +0000

    Added collection ordering functionality

--- lib/Convenient/Data/Collection/NoDb.php
+++ lib/Convenient/Data/Collection/NoDb.php
@@ -1,5 +1,26 @@
 <?php
 class Convenient_Data_Collection_NoDb extends Varien_Data_Collection
 {
+    /**
+     * @param array $collection
+     * @return mixed
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function renderOrders($collection)
+    {
+        $sortOrderDesc = self::SORT_ORDER_DESC;
 
+        foreach (array_reverse($this->_orders, true) as $sortField => $direction) {
+            usort(
+                $collection,
+                function (Varien_Object $data1, Varien_Object $data2) use ($sortField, $direction, $sortOrderDesc) {
+                    $directionMultiplier = !strcasecmp($direction, $sortOrderDesc) ? -1 : 1;
+                    return ($directionMultiplier * strcasecmp($data1[$sortField], $data2[$sortField]));
+                }
+            );
+        }
+
+        return $collection;
+    }
 }
