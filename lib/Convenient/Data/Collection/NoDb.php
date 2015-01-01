50c1d133186a1d9f24b1620be7fad2690a692866
convenient <lukerodgers90@gmail.com>
2015-01-01 21:40:00 +0000

    Added base loadData with beginnings of renderFilters and renderLimit

--- lib/Convenient/Data/Collection/NoDb.php
+++ lib/Convenient/Data/Collection/NoDb.php
@@ -2,6 +2,18 @@
 class Convenient_Data_Collection_NoDb extends Varien_Data_Collection
 {
     protected $orderRenderer = null;
+    protected $dataNoDb = array();
+
+    /**
+     * @return Varien_Data_Collection|void
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function clear()
+    {
+        parent::clear();
+        $this->dataNoDb = array();
+    }
 
     /**
      * @param Convenient_Data_Collection_Renderer_OrderRendererInterface $orderRenderer
@@ -27,13 +39,60 @@ class Convenient_Data_Collection_NoDb extends Varien_Data_Collection
     }
 
     /**
-     * @param array $collection
-     * @return mixed
+     * @return $this
      *
      * @author Luke Rodgers <lukerodgers90@gmail.com>
      */
-    public function renderOrders($collection)
+    protected function loadNoDbData()
     {
-        return $this->getOrderRenderer()->render($collection, $this->_orders);
+        return $this;
+    }
+
+    protected function _renderFilters()
+    {
+        return $this;
+    }
+
+    protected function _renderLimit()
+    {
+        return $this;
+    }
+
+
+    /**
+     * @param bool $printQuery
+     * @param bool $logQuery
+     * @return $this|Varien_Data_Collection
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function loadData($printQuery = false, $logQuery = false)
+    {
+        if ($this->isLoaded()) {
+            return $this;
+        }
+
+        $this->loadNoDbData()
+            ->_renderFilters()
+            ->_renderOrders()
+            ->_renderLimit();
+
+        foreach ($this->dataNoDb as $d) {
+            $this->addItem($d);
+        }
+
+        $this->_setIsLoaded(true);
+        return $this;
+    }
+
+    /**
+     * @return $this|Varien_Data_Collection
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    protected function _renderOrders()
+    {
+        $this->dataNoDb = $this->getOrderRenderer()->render($this->dataNoDb, $this->_orders);
+        return $this;
     }
 }
