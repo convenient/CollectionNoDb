e42e171c85b749477510d6e21dfa7100c095681c
convenient <lukerodgers90@gmail.com>
2015-01-01 21:58:46 +0000

    Refactored to make more like varien_data_collection_db

--- lib/Convenient/Data/Collection/NoDb.php
+++ lib/Convenient/Data/Collection/NoDb.php
@@ -2,18 +2,7 @@
 class Convenient_Data_Collection_NoDb extends Varien_Data_Collection
 {
     protected $orderRenderer = null;
-    protected $dataNoDb = array();
-
-    /**
-     * @return Varien_Data_Collection|void
-     *
-     * @author Luke Rodgers <lukerodgers90@gmail.com>
-     */
-    public function clear()
-    {
-        parent::clear();
-        $this->dataNoDb = array();
-    }
+    protected $data = array();
 
     /**
      * @param Convenient_Data_Collection_Renderer_OrderRendererInterface $orderRenderer
@@ -39,49 +28,25 @@ class Convenient_Data_Collection_NoDb extends Varien_Data_Collection
     }
 
     /**
-     * @return $this
+     * @return array
      *
      * @author Luke Rodgers <lukerodgers90@gmail.com>
      */
-    protected function loadNoDbData()
+    protected function fetchData()
     {
-        return $this;
+        return array();
     }
 
     protected function _renderFilters()
     {
+        if (!$this->_isFiltersRendered) {
+            $this->_isFiltersRendered = true;
+        }
         return $this;
     }
 
     protected function _renderLimit()
     {
-        return $this;
-    }
-
-
-    /**
-     * @param bool $printQuery
-     * @param bool $logQuery
-     * @return $this|Varien_Data_Collection
-     *
-     * @author Luke Rodgers <lukerodgers90@gmail.com>
-     */
-    public function loadData($printQuery = false, $logQuery = false)
-    {
-        if ($this->isLoaded()) {
-            return $this;
-        }
-
-        $this->loadNoDbData()
-            ->_renderFilters()
-            ->_renderOrders()
-            ->_renderLimit();
-
-        foreach ($this->dataNoDb as $d) {
-            $this->addItem($d);
-        }
-
-        $this->_setIsLoaded(true);
         return $this;
     }
 
@@ -92,7 +57,115 @@ class Convenient_Data_Collection_NoDb extends Varien_Data_Collection
      */
     protected function _renderOrders()
     {
-        $this->dataNoDb = $this->getOrderRenderer()->render($this->dataNoDb, $this->_orders);
+        $this->data = $this->getOrderRenderer()->render($this->data, $this->_orders);
         return $this;
     }
+
+    /**
+     * @param bool $printQuery
+     * @param bool $logQuery
+     * @return $this|Varien_Data_Collection
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function loadData($printQuery = false, $logQuery = false)
+    {
+        return $this->load($printQuery, $logQuery);
+
+    }
+
+    /**
+     * @return $this
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    protected function _beforeLoad()
+    {
+        return $this;
+    }
+
+    /**
+     * @return $this
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    protected function _afterLoad()
+    {
+        return $this;
+    }
+
+    /**
+     * @return $this
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    protected function _afterLoadData()
+    {
+        return $this;
+    }
+
+    /**
+     * @return $this
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function resetData()
+    {
+        $this->data = array();
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function getData()
+    {
+        if ($this->data === array()) {
+            $this->_renderFilters()
+                ->_renderOrders()
+                ->_renderLimit();
+            $this->data = $this->fetchData();
+            $this->_afterLoadData();
+        }
+        return $this->data;
+    }
+
+    /**
+     * @param bool $printQuery
+     * @param bool $logQuery
+     * @return $this|Varien_Data_Collection
+     * @throws Exception
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function load($printQuery = false, $logQuery = true)
+    {
+        if ($this->isLoaded()) {
+            return $this;
+        }
+
+        $this->_beforeLoad();
+
+        $this->getData();
+
+        $this->_renderFilters()
+            ->_renderOrders()
+            ->_renderLimit();
+
+        $data = $this->getData();
+        $this->resetData();
+
+        if (is_array($data)) {
+            foreach ($data as $row) {
+                $this->addItem($row);
+            }
+        }
+
+        $this->_setIsLoaded();
+        $this->_afterLoad();
+        return $this;
+    }
+
 }
