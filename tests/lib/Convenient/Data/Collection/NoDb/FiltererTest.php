a0777758b8459b202206d5373a774519c2a84ade
convenient <lukerodgers90@gmail.com>
2015-01-04 18:17:58 +0000

    Tested numeric filtering

--- tests/lib/Convenient/Data/Collection/NoDb/FiltererTest.php
+++ tests/lib/Convenient/Data/Collection/NoDb/FiltererTest.php
@@ -19,6 +19,126 @@ class Convenient_Data_Collection_NoDb_FiltererTest extends PHPUnit_Framework_Tes
      * @param $expected
      *
      * @test
+     * @dataProvider fromToProvider
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function fromTo($collection, $filters, $expected)
+    {
+        $this->assertEquals($expected, $this->filterer->filter($collection, $filters));
+    }
+
+    /**
+     * @return array
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function fromToProvider()
+    {
+        $rowA = new Varien_Object(
+            array(
+                'field' => 3
+            )
+        );
+
+        $rowB = new Varien_Object(
+            array(
+                'field' => 6
+            )
+        );
+
+        $rowC = new Varien_Object(
+            array(
+                'field' => 9
+            )
+        );
+
+        return array(
+            array(
+                array($rowA, $rowB, $rowC),
+                array(
+                    'field' => array(
+                        new Varien_Object(
+                            array(
+                                'value' => array(
+                                    'from' => 6
+                                )
+                            )
+                        )
+                    )
+                ),
+                array($rowB, $rowC)
+            ),
+            array(
+                array($rowA, $rowB, $rowC),
+                array(
+                    'field' => array(
+                        new Varien_Object(
+                            array(
+                                'value' => array(
+                                    'from' => 7
+                                )
+                            )
+                        )
+                    )
+                ),
+                array($rowC)
+            ),
+            array(
+                array($rowA, $rowB, $rowC),
+                array(
+                    'field' => array(
+                        new Varien_Object(
+                            array(
+                                'value' => array(
+                                    'to' => 7
+                                )
+                            )
+                        )
+                    )
+                ),
+                array($rowA, $rowB)
+            ),
+            array(
+                array($rowA, $rowB, $rowC),
+                array(
+                    'field' => array(
+                        new Varien_Object(
+                            array(
+                                'value' => array(
+                                    'to' => 6
+                                )
+                            )
+                        )
+                    )
+                ),
+                array($rowA, $rowB)
+            ),
+            array(
+                array($rowA, $rowB, $rowC),
+                array(
+                    'field' => array(
+                        new Varien_Object(
+                            array(
+                                'value' => array(
+                                    'from' => 6,
+                                    'to' => 9
+                                )
+                            )
+                        )
+                    )
+                ),
+                array($rowB, $rowC)
+            ),
+        );
+    }
+
+    /**
+     * @param $collection
+     * @param $filters
+     * @param $expected
+     *
+     * @test
      * @dataProvider likeProvider
      *
      * @author Luke Rodgers <lukerodgers90@gmail.com>
