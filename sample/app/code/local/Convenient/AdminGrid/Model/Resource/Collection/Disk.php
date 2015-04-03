e78b7de268d79e2412944bd7335d4b4021540898
convenient <lukerodgers90@gmail.com>
2015-04-03 22:22:59 +0100

    Added caching to the sample

--- sample/app/code/local/Convenient/AdminGrid/Model/Resource/Collection/Disk.php
+++ sample/app/code/local/Convenient/AdminGrid/Model/Resource/Collection/Disk.php
@@ -14,30 +14,75 @@ class Convenient_AdminGrid_Model_Resource_Collection_Disk extends Convenient_Dat
     protected function fetchData()
     {
         if (is_null($this->objectCachedData)) {
-            $data = array();
+            $data = $this->getFromCache();
 
-            $handle = fopen('data.csv', 'r');
-            if (!$handle) {
-                die("Failed to open file");
+            if (!$data) {
+                $data = array();
+
+                $handle = fopen('data.csv', 'r');
+                if (!$handle) {
+                    die("Failed to open file");
+                }
+
+                $headersData = fgetcsv($handle);
+                if ($headersData == false) {
+                    die("Failed to load file headers");
+                }
+
+                while (($line = fgetcsv($handle)) !== false) {
+                    $temp = array_combine($headersData, $line);
+
+                    $item = $this->getNewEmptyItem();
+                    $item->addData($temp);
+                    $data[] = $item;
+                }
+
+                fclose($handle);
+
+                $this->saveToCache($data);
             }
-
-            $headersData = fgetcsv($handle);
-            if ($headersData == false) {
-                die("Failed to load file headers");
-            }
-
-            while (($line = fgetcsv($handle)) !== false) {
-                $temp = array_combine($headersData, $line);
-
-                $item = $this->getNewEmptyItem();
-                $item->addData($temp);
-                $data[] = $item;
-            }
-
-            fclose($handle);
-
+            
             $this->objectCachedData = $data;
         }
         return $this->objectCachedData;
+    }
+
+    /**
+     * @return bool|mixed
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function getFromCache()
+    {
+        $data = $this->getCacheInstance()->load('convenient_admingrid_collection_disk_access');
+        if ($data) {
+            return unserialize($data);
+        }
+        return false;
+    }
+
+    /**
+     * @param $data
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    public function saveToCache($data)
+    {
+        $this->getCacheInstance()->save(
+            serialize($data),
+            'convenient_admingrid_collection_disk_access',
+            array(),
+            3600
+        );
+    }
+
+    /**
+     * @return Mage_Core_Model_Cache
+     *
+     * @author Luke Rodgers <lukerodgers90@gmail.com>
+     */
+    protected function getCacheInstance()
+    {
+        return Mage::app()->getCacheInstance();
     }
 }
